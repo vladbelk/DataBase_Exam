121f6c8e3ee484d624ad8149e21028070de69c70
vladbelk <vladbelk321@gmail.com>
2024-04-12 15:06:37 +0300

    script_insert

--- script_insert_ex.sql
+++ script_insert_ex.sql
@@ -54,11 +54,11 @@ VALUES
     ('Whispers in the Shadows', 250, 15.99, '2022-03-03', 2, 2, 2),
     ('Galactic Odyssey', 400, 30.00, '2022-04-04', 3, 3, 3),
     ('Realm of Enchantment', 350, 20.50, '2022-05-05', 4, 4, 4),
-    ('Loves Eternal Dance', 280, 18.75, '2022-06-06', 5, 5, 5),
+    ('Loves Eternal Dance', 550, 18.75, '2022-06-06', 5, 5, 5),
     ('Nightmare Chronicles', 320, 22.80, '2022-07-07', 6, 6, 6),
     ('Edge of Suspense', 270, 17.25, '2022-08-08', 7, 7, 7),
     ('Biography of a Visionary', 400, 28.50, '2022-09-09', 8, 8, 8),
-    ('Chronicles of History', 380, 26.00, '2022-10-10', 9, 9, 9),
+    ('Chronicles of History', 580, 26.00, '2022-10-10', 9, 9, 9),
     ('Verses of the Soul', 220, 15.00, '2022-11-11', 10, 10, 10);
 INSERT INTO Shops (Id, Name, CountryId)
 VALUES
